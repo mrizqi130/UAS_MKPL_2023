6a58b5db628c8f0fccb259316c589942e3191785
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2023-06-11 11:30:12 +0700

    tambah test

---
+++ test/exterminators/ExterminatorsTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exterminators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ExterminatorsTest {
+    
+    public ExterminatorsTest() {
+    }
+    /**
+     * Test of main method, of class Exterminators.
+     */
+    @Test
+    public void testMain() {
+//        System.out.println("main");
+        String[] args = null;
+//        Exterminators.main(args);
+        Exterminators.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
